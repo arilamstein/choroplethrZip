d4b758f4be55d7f71bc64b12c804e0e4d4c74bee
Ari Lamstein <arilamstein@gmail.com>
2015-03-04 09:54:26 -0800

    Rename zcta -> zip

--- R/data.R
+++ R/data.R
@@ -1,14 +1,14 @@
-#' Map of Zip Code Tabulated Areas (ZCTAs) of the 50 US States plus the District of Columbia
+#' data.frame representation of a shapefile of US ZIP Codes
 #' 
-#' A data.frame which contains a map of the Zip Code Tabulated Areas (ZCTAs) of all 50 states plus the District
-#' of Columbia. ZCTAs are the US Census Bureau's representation of US ZIP Codes.  The shapefile
+#' The map is of Zip Code Tabulated Areas (ZCTAs) of the 50 US States plus the District of Columbia and
+#' comes from the US Census Bureau. The shapefile
 #' was modified using QGIS in order to remove Puerto Rico, islands off of Alaska that
 #' crossed the antimeridian and American Samoa. A column named "region" was added for convenience which
 #' is a duplicate of the column named ZCTA5CE10.
 #'
 #' @docType data
-#' @name zcta.map
-#' @usage data(zcta.map)
+#' @name zip.map
+#' @usage data(zip.map)
 #' @references Taken from the US Census 2013 Cartographic Boundary Shapefiles - ZIP Code Tabulation Areas (ZCTAs): 
 #' https://www.census.gov/geo/maps-data/data/cbf/cbf_zcta.html. 
 #' @examples
@@ -16,19 +16,19 @@
 #' # render the map with ggplot2
 #' library(ggplot2)
 #' 
-#' data(zcta.map)
-#' ggplot(zcta.map, aes(long, lat, group=group)) + geom_polygon()
+#' data(zip.map)
+#' ggplot(zip.map, aes(long, lat, group=group)) + geom_polygon()
 #' }
 NULL
 
 #' A data.frame consisting of the name of each region in the map zcta.map.
 #' 
-#' @seealso ?zcta.map
+#' @seealso ?zip.map
 #'  
 #' @docType data
-#' @name zcta.regions
-#' @usage data(zcta.regions)
+#' @name zip.regions
+#' @usage data(zip.regions)
 #' @examples
-#' data(zcta.regions)
-#' head(zcta.regions) 
+#' data(zip.regions)
+#' head(zip.regions) 
 NULL
