011da15f49bb7573c7bfc37672c3efa8c92139e2
Ari Lamstein <arilamstein@gmail.com>
2015-03-15 16:32:33 -0700

    suppress warnings here bc census just doesn't have all the data

--- inst/tests/test-zip_choropleth_acs.R
+++ inst/tests/test-zip_choropleth_acs.R
@@ -2,20 +2,20 @@ context("test zip_choropleth_acs")
 
 test_that("zip_choropleth_acs returns a ggplot2 with state_zoom set", {
   expect_is(
-    zip_choropleth_acs("B19301", state_zoom="new york"),
+    suppressWarnings(zip_choropleth_acs("B19301", state_zoom="new york")),
     "ggplot")
 })
 
 test_that("zip_choropleth_acs returns a ggplot2 with county_zoom set", {
   nyc_fips = c(36005, 36047, 36061, 36081, 36085)
   expect_is(
-    zip_choropleth_acs("B19301", county_zoom=nyc_fips),
+    suppressWarnings(zip_choropleth_acs("B19301", county_zoom=nyc_fips)),
     "ggplot")
 })
 
 test_that("zip_choropleth_acs returns a ggplot2 with msa_zoom set", {
   expect_is(
-    zip_choropleth_acs("B19301", num_colors=1, msa_zoom="New York-Newark-Jersey City, NY-NJ-PA"),
+    suppressWarnings(zip_choropleth_acs("B19301", num_colors=1, msa_zoom="New York-Newark-Jersey City, NY-NJ-PA")),
     "ggplot")
 })
 
@@ -24,6 +24,6 @@ test_that("zip_choropleth_acs returns a ggplot2 with zip_zoom set", {
   manhattan_les = c("10002", "10003", "10009")
   manhattan_ues = c("10021", "10028", "10044", "10128")
   expect_is(
-    zip_choropleth_acs("B19301", num_colors=1, zip_zoom=c(manhattan_les, manhattan_ues)),
+    suppressWarnings(zip_choropleth_acs("B19301", num_colors=1, zip_zoom=c(manhattan_les, manhattan_ues))),
     "ggplot")
 })
