d492069f781e1e248e63134b51ee4596353a9ef9
Ari Lamstein <arilamstein@gmail.com>
2018-08-23 14:01:15 -0700

    pkgdown

---
+++ pkgdown/extra.js
@@ -0,0 +1,13 @@
+// Drip 
+var _dcq = _dcq || [];
+var _dcs = _dcs || {};
+_dcs.account = '3446315';
+
+(function() {
+  var dc = document.createElement('script');
+  dc.type = 'text/javascript'; dc.async = true;
+  dc.src = '//tag.getdrip.com/3446315.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(dc, s);
+})();
+// end Drip
